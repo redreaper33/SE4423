ead22a20d9a56840134ccc4a32d1948e1186959e
Kyle <kmiller48@uco.edu>
2018-11-10 20:57:47 -0600

    commit back to start for merge testing

--- SE4423/src/Hello.java
+++ SE4423/src/Hello.java
@@ -7,8 +7,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		System.out.println(“The conflict is solved!”);
-		System.out.println(“A bug is fixed on bug_fixing branch!”);
 
 	}
 
